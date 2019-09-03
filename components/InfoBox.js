093810aa018f0863eb9014e9b2b78ea8cbf221f8
Shamim <mdshamim723@gmail.com>
2019-09-03 15:53:25 +0600

    Info Box Added

---
+++ components/InfoBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InfoBox = ({ icon, text }) => {
+    return (
+        <p><i>icon</i> {text}</p>
+    );
+}
+
+export default InfoBox;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import Brand from '../components/brand'
+import Brand from '../components/Brand'
+import InfoBox from '../components/InfoBox';
 
 const Home = () => (
   <Brand brandName="Vivasoft"></Brand>
+  <InfoBox icon="icon" text="text"></InfoBox>
 )
 
 export default Home
