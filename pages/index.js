25297b50256eb86771f795b36b81330d7ce943a6
Shamim <mdshamim723@gmail.com>
2019-09-03 16:00:02 +0600

    Info Box Added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Brand from '../components/Brand'
 import InfoBox from '../components/InfoBox';
 
 const Home = () => (
-  <Brand brandName="Vivasoft"></Brand>
-  <InfoBox icon="icon" text="text"></InfoBox>
+  <Fragment>
+    <Brand brandName="Vivasoft"></Brand>
+    <InfoBox icon="icon" text="text"></InfoBox>
+  </Fragment>
 )
 
 export default Home
