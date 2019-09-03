221cdf739170933b60b4fc3a9f5f4ce45e47d807
Shamim <mdshamim723@gmail.com>
2019-09-03 15:44:22 +0600

    Initial Commit

---
+++ components/brand.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Brand = ({ brandName }) => {
+  return (
+    <h2>{brandName}</h2>
+  );
+}
+
+export default Brand;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Brand from '../components/brand'
+
+const Home = () => (
+  <Brand brandName="Vivasoft"></Brand>
+)
+
+export default Home
