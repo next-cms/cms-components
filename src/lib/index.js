a9037728b23eeacfb6a5a4f173f6be837697d2dd
tahsinsiad <tak.siad16@gmail.com>
2019-10-03 15:01:25 +0600

    mobile nav

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
 import InfoBox from './components/InfoBox';
 import Brand from './components/Brand';
 //import PricingTable from './components/PricingTable';
-import Nav from './components/NavBar';
+import Nav from './components/NavBar2';
 
 
 export { InfoBox, Brand, Nav };
