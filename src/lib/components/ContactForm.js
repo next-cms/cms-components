68e575548af837e16b88c735de0f8a6e6529929b
unknown <tak.siad16@gmail.com>
2019-10-07 01:13:28 +0600

    web version contact form completed

---
+++ src/lib/components/ContactForm.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "../../../static/contactForm.css";
+import { Icon } from "antd";
+
+const ContactForm = () => {
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="contact-form">
+          <div className="partner">
+            <div className="partner-details">
+              <h1>Become a partner</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                mollis odio mattis, lobortis leo id, bibendum nulla. Nunc eu
+                augue purus. Mauris vehicula aliquam mi sed iaculis. Praesent at
+                dapibus odio. Sed cursus pulvinar bibendum. Fusce consequat
+                lorem vitae mauris consectetur, sit amet dapibus dui gravida.
+                Nunc vel ipsum sodales urna mattis eleifend a at dui. Vestibulum
+                suscipit tristique gravida.
+              </p>
+            </div>
+            <div className="partner-info">
+              <div className="partner-img">
+                <img
+                  src="../../../static/Band-Logo.png"
+                  style={{ width: "100%" }}
+                />
+              </div>
+              <div>
+                <ul>
+                  <li>Partner Director</li>
+                  <li>
+                    <b>FIRSTNAME LASTNAME</b>
+                  </li>
+                  <li>+358 40 123 12344</li>
+                </ul>
+                <div className="social-icons">
+                  <div>
+                    <Icon type="linkedin" style={{ fontSize: "32px" }} />
+                  </div>
+                  <div>
+                    <Icon type="twitter" style={{ fontSize: "32px" }} />
+                  </div>
+                  <div>
+                    <Icon type="facebook" style={{ fontSize: "32px" }} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="form">
+            <div className="form-details">
+              <div>
+                <h4>INTERESTED IN BECOMING A PARTNER?</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  mollis odio mattis, lobortis leo id, bibendum nulla. Nunc eu
+                  augue purus. Mauris vehicula aliquam mi sed iaculis. Praesent
+                  at dapibus odio.
+                </p>
+              </div>
+              <label>NAME</label>
+              <br />
+              <input type="text" name="name" />
+              <br />
+              <label>COMPANY</label>
+              <br />
+              <input type="text" name="company" />
+              <br />
+              <label>PHONE</label>
+              <br />
+              <input type="text" name="phone" />
+              <br />
+              <label>EMAIL</label>
+              <br />
+              <input type="email" name="email" />
+              <br />
+              <div className="submit-btn">
+                <button className=" primary-button">SUBMIT</button>
+              </div>
+            </div>
+          </div>
+          <div>image</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/lib/components/NavBar2.js
+++ src/lib/components/NavBar2.js
@@ -1,130 +1,142 @@
-import React, { useState } from 'react'
-import "../../../static/navbar2.css"
+import React, { useState, Fragment } from "react";
+import "../../../static/navbar2.css";
 import { Icon } from "antd";
+import ContactForm from "./ContactForm";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
+  const toggleMobileNav = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggleMobileNav = () => {
+  return (
+    <Fragment>
+      <header className="header">
+        <div className="container main-menu">
+          <div className="logo">
+            <img style={{ width: "100%" }} src="../../static/Band-Logo.png" />
+          </div>
 
-        setIsOpen(!isOpen);
-
-    }
-
-    return (
-        <header className="header">
-            <div className="container main-menu">
-                <div className="logo">
-                    <img style={{ width: "100%" }} src="../../static/Band-Logo.png" />
-                </div>
-
-                <div className="nav-menu">
-
-                    <div className="nav-item nav-one">
-                        <ul>
-                            <li>
-                                <a href="#">PLATFORM OF TRUST</a>
-                            </li>
-                            <li>
-                                <a href="#">DEVELOPER PORTAL </a>
-                            </li>
-                            <li>
-                                <a href="#">SERVICE STATUS</a>
-                            </li>
-                        </ul>
-                        <div className="extra">
-                            <a className="button">SOUMEKSI</a>
-                            <a className="button" style={{ marginRight: "37px" }}>IN ENGLISH</a>
-                            <button className="secondary-btn">SIGN IN / REGISTER</button>
-                        </div>
-                    </div>
-
-                    <div className="nav-item nav-two">
-                        <ul>
-                            <li>
-                                <a href="#">CASES</a>
-                            </li>
-                            <li>
-                                <a href="#">PARTNERS</a>
-                            </li>
-                            <li>
-                                <a href="#">FAQ</a>
-                            </li>
-                            <li>
-                                <a href="#">NEWS</a>
-                            </li>
-                            <li>
-                                <a href="#">EVENTS</a>
-                            </li>
-                            <li>
-                                <a href="#">ABOUT</a>
-                            </li>
-                            <li>
-                                <a href="#">CONTACT</a>
-                            </li>
-                        </ul>
-                        <div className="extra">
-                            <button className="primary-button">PRICING</button>
-                        </div>
-                    </div>
-                </div>
-
-                <Icon className="bar" onClick={toggleMobileNav} type="menu" style={{ fontSize: "30px", color: "white" }} />
+          <div className="nav-menu">
+            <div className="nav-item nav-one">
+              <ul>
+                <li>
+                  <a href="#">PLATFORM OF TRUST</a>
+                </li>
+                <li>
+                  <a href="#">DEVELOPER PORTAL </a>
+                </li>
+                <li>
+                  <a href="#">SERVICE STATUS</a>
+                </li>
+              </ul>
+              <div className="extra">
+                <a className="button">SOUMEKSI</a>
+                <a className="button" style={{ marginRight: "37px" }}>
+                  IN ENGLISH
+                </a>
+                <button className="secondary-btn">SIGN IN / REGISTER</button>
+              </div>
             </div>
 
-            <div className={`mobile-menu ${isOpen ? 'active-mobile-nav' : ''}`}>
-                <div className="mobile-menu-content">
-                    <div className="nav-one-mobile-menu container">
-                        <Icon className="close-icon" onClick={toggleMobileNav} type="close" />
-                        <h4 className="mobile-logo-text">PLATFORM OF TRUST</h4>
-                    </div>
+            <div className="nav-item nav-two">
+              <ul>
+                <li>
+                  <a href="#">CASES</a>
+                </li>
+                <li>
+                  <a href="#">PARTNERS</a>
+                </li>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">NEWS</a>
+                </li>
+                <li>
+                  <a href="#">EVENTS</a>
+                </li>
+                <li>
+                  <a href="#">ABOUT</a>
+                </li>
+                <li>
+                  <a href="#">CONTACT</a>
+                </li>
+              </ul>
+              <div className="extra">
+                <button className="primary-button">PRICING</button>
+              </div>
+            </div>
+          </div>
 
-                    <div className="nav-two-mobile-menu">
-                        <ul>
-                            <li>
-                                <a href="#">CASES</a>
-                            </li>
-                            <li>
-                                <a href="#">PARTNERS</a>
-                            </li>
-                            <li>
-                                <a href="#">FEATURES</a>
-                            </li>
-                            <li>
-                                <a href="#">NEWS</a>
-                            </li>
-                            <li>
-                                <a href="#">EVENTS</a>
-                            </li>
-                            <li>
-                                <a href="#">ABOUT</a>
-                            </li>
-                            <li>
-                                <a href="#">CONTACT US</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="nav-three-mobile-menu">
-                        <ul>
-                            <li>
-                                <a href="#">DEVELOPER PORTAL </a>
-                            </li>
-                            <li>
-                                <a href="#">SERVICE STATUS</a>
-                            </li>
-                            <li>
-                                <a href="#">SOUMEKSI</a>
-                            </li>
-                        </ul>
-                        <div className="extra">
-                            <button className="secondary-btn btn-lg">SIGN IN / REGISTER</button>
-                        </div>
-                    </div>
-                </div>
+          <Icon
+            className="bar"
+            onClick={toggleMobileNav}
+            type="menu"
+            style={{ fontSize: "30px", color: "white" }}
+          />
+        </div>
+
+        <div className={`mobile-menu ${isOpen ? "active-mobile-nav" : ""}`}>
+          <div className="mobile-menu-content">
+            <div className="nav-one-mobile-menu container">
+              <Icon
+                className="close-icon"
+                onClick={toggleMobileNav}
+                type="close"
+              />
+              <h4 className="mobile-logo-text">PLATFORM OF TRUST</h4>
             </div>
 
-        </header>
-    )
-}
+            <div className="nav-two-mobile-menu">
+              <ul>
+                <li>
+                  <a href="#">CASES</a>
+                </li>
+                <li>
+                  <a href="#">PARTNERS</a>
+                </li>
+                <li>
+                  <a href="#">FEATURES</a>
+                </li>
+                <li>
+                  <a href="#">NEWS</a>
+                </li>
+                <li>
+                  <a href="#">EVENTS</a>
+                </li>
+                <li>
+                  <a href="#">ABOUT</a>
+                </li>
+                <li>
+                  <a href="#">CONTACT US</a>
+                </li>
+              </ul>
+            </div>
+            <div className="nav-three-mobile-menu">
+              <ul>
+                <li>
+                  <a href="#">DEVELOPER PORTAL </a>
+                </li>
+                <li>
+                  <a href="#">SERVICE STATUS</a>
+                </li>
+                <li>
+                  <a href="#">SOUMEKSI</a>
+                </li>
+              </ul>
+              <div className="extra">
+                <button className="secondary-btn btn-lg">
+                  SIGN IN / REGISTER
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <ContactForm />
+    </Fragment>
+  );
+};
 export default Nav;
